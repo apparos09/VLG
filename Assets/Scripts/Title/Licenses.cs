6ad1797a59455b1988428f2705e6fd6e6f3430dd
mecha-rm <roderick.montague243@gmail.com>
2024-07-21 18:33:57 -0400

    Added Space for Font Credits
    Space has been added to credit fonts that will be added to the game later.

--- Assets/Scripts/Title/Licenses.cs
+++ Assets/Scripts/Title/Licenses.cs
@@ -24,6 +24,12 @@ namespace VLG
         // The sound effects credit page.
         public int sfxCreditsIndex = 0;
 
+        // The font credits for the game.
+        public AudioCredits fontsCredits;
+
+        // The fonts credit page.
+        public int fontsCreditsIndex = 0;
+
         [Header("UI")]
 
         // BGM Button
@@ -31,6 +37,9 @@ namespace VLG
 
         // SFX Button
         public Button sfxButton;
+
+        // Fonts Button
+        public Button fontsButton;
 
         // Start is called before the first frame update
         void Start()
@@ -63,6 +72,32 @@ namespace VLG
             // TODO: load SFX credits
         }
 
+        // Loads the font credits.
+        private void LoadFontCredits()
+        {
+            AudioCredits.AudioCredit credit = new AudioCredits.AudioCredit();
+
+            // TODO: load SFX credits
+        }
+
+        // Enables all the credit buttons.
+        public void EnableAllCreditButtons()
+        {
+            // Disables all the credit buttons.
+            bgmButton.interactable = true;
+            sfxButton.interactable = true;
+            fontsButton.interactable = true;
+        }
+
+        // Disables all the credit buttons.
+        public void DisableAllCreditButtons()
+        {
+            // Disables all the credit buttons.
+            bgmButton.interactable = false;
+            sfxButton.interactable = false;
+            fontsButton.interactable = false;
+        }
+
         // Shows the BGM credits.
         public void ShowBackgroundMusicCredits()
         {
@@ -72,8 +107,8 @@ namespace VLG
             creditsInterface.SetCreditIndex(bgmCreditsIndex);
 
             // Change button settings.
+            EnableAllCreditButtons();
             bgmButton.interactable = false;
-            sfxButton.interactable = true;
         }
 
         // Shows the SFX credits.
@@ -85,8 +120,21 @@ namespace VLG
             creditsInterface.SetCreditIndex(sfxCreditsIndex);
 
             // Change button settings.
-            bgmButton.interactable = true;
+            EnableAllCreditButtons();
             sfxButton.interactable = false;
+        }
+
+        // Shows the font credits.
+        public void ShowFontCredits()
+        {
+            // Saves the current credit index, switches over, and then sets the new credit index.
+            fontsCreditsIndex = creditsInterface.GetCurrentCreditIndex();
+            creditsInterface.audioCredits = fontsCredits;
+            creditsInterface.SetCreditIndex(fontsCreditsIndex);
+
+            // Change button settings.
+            EnableAllCreditButtons();
+            fontsButton.interactable = false;
         }
     }
 }
