441f328b8826972de46458b4d3b940267624e972
mecha-rm <roderick.montague243@gmail.com>
2024-07-26 17:50:06 -0400

    Fixed Final Boss Deletion Glitch
    Due to the way the game is set up, the final boss can't be an instance, otherwise it gets deleted. This is because the old one is not removed from memory before the new one is made, causing both of them to get deleted since the program thinks one already exists. This has been fixed.

--- Assets/Scripts/Gameplay/Floor/Entities/Enemies/FinalBoss.cs
+++ Assets/Scripts/Gameplay/Floor/Entities/Enemies/FinalBoss.cs
@@ -7,13 +7,6 @@ namespace VLG
     // The final boss of the game.
     public class FinalBoss : Boss
     {
-        // The singleton instance.
-        private static FinalBoss instance;
-
-        // Gets set to 'true' when the singleton has been instanced.
-        // This isn't needed, but it helps with the clarity.
-        private static bool instanced = false;
-
         [Header("Final Boss")]
 
         // The lighting strike prefab
@@ -31,26 +24,7 @@ namespace VLG
         // Awake is called when the script is being loaded
         protected override void Awake()
         {
-            // If the instance hasn't been set, set it to this object.
-            if (instance == null)
-            {
-                instance = this;
-            }
-            // If the instance isn't this, destroy the game object.
-            else if (instance != this)
-            {
-                Destroy(gameObject);
-                return;
-            }
-
-            // Run code for initialization.
-            if (!instanced)
-            {
-                instanced = true;
-
-                // Base Awake
-                base.Awake();
-            }
+            base.Awake();
         }
 
         // Start is called before the first frame update
@@ -67,42 +41,6 @@ namespace VLG
             // Lighting Strike Test - Make Sure to Remove
             SpawnLightingStrike(new Vector2Int(6, 0));
 
-        }
-
-        // Gets the instance.
-        public static FinalBoss Instance
-        {
-            get
-            {
-                // Checks if the instance exists.
-                if (instance == null)
-                {
-                    // Tries to find the instance.
-                    instance = FindObjectOfType<FinalBoss>(true);
-
-
-                    // The instance doesn't already exist.
-                    if (instance == null)
-                    {
-                        // Generate the instance.
-                        GameObject go = new GameObject("FinalBoss (singleton)");
-                        instance = go.AddComponent<FinalBoss>();
-                    }
-
-                }
-
-                // Return the instance.
-                return instance;
-            }
-        }
-
-        // Returns 'true' if the object has been initialized.
-        public static bool Instantiated
-        {
-            get
-            {
-                return instanced;
-            }
         }
 
         // Spawns a lighting strike at the provided position.
@@ -175,12 +113,6 @@ namespace VLG
         protected override void OnDestroy()
         {
             base.OnDestroy();
-
-            // If the saved instance is being deleted, set 'instanced' to false.
-            if (instance == this)
-            {
-                instanced = false;
-            }
         }
 
     }
