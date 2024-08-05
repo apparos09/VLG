8e9c161473f92d80922f91a663b51181b78b8118
mecha-rm <roderick.montague243@gmail.com>
2024-08-05 13:39:29 -0400

    Added Tutorial Images + Text Box Script
    Tutorial images have been added to the game, and text box scripts have been added as well. No implementation yet.

---
+++ Assets/Scripts/Utilities/Text Box/Page.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace util
+{
+    // A page in a textbox.
+    public class Page
+    {
+        // The text for the page.
+        public string text = "";
+
+        // The key for the text to be read with text-to-speech. This may differ from what the text actually says.
+        public string speakKey = "";
+
+        // The callback for the page.
+        public delegate void PageCallback();
+
+        // The callback for opening the page.
+        private PageCallback pageOpenCallback;
+
+        // The callback for closing the page.
+        private PageCallback pageCloseCallback;
+
+        // Generates the empty page.
+        public Page()
+        {
+            text = "";
+            speakKey = "";
+        }
+
+        // Adds a page with text.
+        public Page(string text)
+        {
+            this.text = text;
+            speakKey = "";
+        }
+
+        // Adds a page with text and a speak key.
+        public Page(string text, string speakKey)
+        {
+            this.text = text;
+            this.speakKey = speakKey;
+        }
+
+        // Reads the page.
+        public void SpeakPage()
+        {
+            // TODO: read page using TTS
+        }
+
+        // Stops the page from being spoken.
+        public void StopSpeakingPage()
+        {
+            // TODO: stop reading page using TTS
+        }
+
+        // Add a callback for when the page is opened.
+        public void OnPageOpenedAddCallback(PageCallback callback)
+        {
+            pageOpenCallback += callback;
+        }
+
+        // Remove a callback for when the page is opened.
+        public void OnPageOpenedRemoveCallback(PageCallback callback)
+        {
+            pageOpenCallback -= callback;
+        }
+
+        // Add a callback for when the page is closd.
+        public void OnPageClosedAddCallback(PageCallback callback)
+        {
+            pageCloseCallback += callback;
+        }
+
+        // Remove a callback for when the page is closed.
+        public void OnPageClosedRemoveCallback(PageCallback callback)
+        {
+            pageCloseCallback -= callback;
+        }
+
+        // Called when the page is opened.
+        public virtual void OnPageOpened()
+        {
+            // Trigger the callbacks.
+            if (pageOpenCallback != null)
+                pageOpenCallback();
+
+            // Use text-to-speech to speak the page content.
+            SpeakPage();
+        }
+
+        // Called when the page is closed.
+        public virtual void OnPageClosed()
+        {
+            // Trigger the callbacks.
+            if (pageCloseCallback != null)
+                pageCloseCallback();
+        }
+    }
+}
